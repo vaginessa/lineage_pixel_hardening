84496e7d6ce924c5d87984d5bd791d2ac66b9305
danielk43 <11856117+danielk43@users.noreply.github.com>
2023-02-16 19:33:00 -0500

    Don't fail on unbound vars

--- dos_apply.sh
+++ dos_apply.sh
@@ -15,7 +15,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <https://www.gnu.org/licenses/>.
 umask 0022;
-set -euo pipefail;
+set -eo pipefail;
 
 export DOS_WORKSPACE_ROOT=${GIT_LOCAL}"/DivestOS-Build"; #XXX: THIS MUST BE CORRECT TO PATCH
 source "${DOS_WORKSPACE_ROOT}/Scripts/init.sh"
